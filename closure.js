e77bc9c196b340a115ff39ada53a9d33faa82320
Mikail Bakulin <bakulin.mv@gmail.com>
2022-08-18 17:45:13 +0300

    Initial commit

---
+++ closure.js
@@ -0,0 +1,13 @@
+function getPasswordChecker(passRight) {
+    // let checkPass = passRight;
+    return function checkPass(passCheck) {
+        const pass = passRight;
+        result = pass == passCheck;
+        return result;      
+    }
+    
+} 
+const answ1 = getPasswordChecker(333);
+console.log(answ1(111))
+console.log(answ1(1112))
+console.log(answ1(333))
