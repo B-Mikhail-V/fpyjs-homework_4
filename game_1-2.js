9a0e7113020d33eadfac78b1951530016f54391c
Mikail Bakulin <bakulin.mv@gmail.com>
2022-08-24 17:34:49 +0300

    game V6

--- game_1-2.js
+++ game_1-2.js
@@ -4,50 +4,17 @@ const rl = readline.createInterface({ input, output });
 
 
 let num = Math.floor(Math.random() * 1000);
-console.log(num);
+// console.log(num);
 let count = 0;
 
 function findNumber (callback) {
-    // while (true) {
         count ++;
         rl.question("Введите целое число от 0 до 999, для выхода - q. ПОПЫТКА № " + count + ": ", answer => {
-            console.log('Вы ввели', answer);
-            // rl.close();
+            console.log('--------------\nВы ввели', answer, '\n--------------');
             callback(answer);
         })
-
-    // }
-
 }
 
-
-// rl.close;
-
-
-// while (true) {
-//     let guessNum = prompt('Введите целое число от 0 до 999, для выхода - q:');
-//     console.log(guessNum)
-//     count ++
-//     if (guessNum == 'q') {
-//         alert('Заходите еще попытать счастье, пока!');
-//         break;
-//     } else if (+guessNum === num) {
-//         alert('Вы угадали! Количество попыток -' + count);
-//         break;
-//     }
-
-    // if (isNaN(guessNum)) {
-    //     alert('Вы ввели не число!')
-    // } else if (guessNum > 999 || guessNum < 0) {
-    //     alert('Вы ввели число за пределами предлагаемого диапазона!')
-    // } else if (guessNum != Math.floor(guessNum)) {
-    //     alert('Вы ввели не целое число!')
-    // } else if (guessNum - num > 0) {
-    //     alert ('Загаданное число меньше Вашего числа, пока не угадали!')
-    // } else if (guessNum - num < 0) {
-    //     alert ('Загаданное число больше Вашего числа, пока не угадали!')
-    // } 
-// }
 
 function checkError (guessNum) {
     if (isNaN(guessNum) && guessNum !== 'q') {
@@ -72,11 +39,10 @@ function checkError (guessNum) {
         console.log('Заходите еще попытать счастье, пока!');
         rl.close();
     } else if (+guessNum === num) {
-        console.log('Вы угадали! Количество попыток -', count);
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nВы угадали! Количество попыток -', count, '\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
         rl.close();
     }
         
-
 }
 
 
